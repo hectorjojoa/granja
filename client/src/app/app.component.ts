19ab81fc0bb6ab4946c14e0704007e5de705eaac
Hector Jojoa <hectorjojoa91@gmail.com>
2024-05-13 14:44:42 -0500

    registro_usuario_ok

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -17,15 +17,25 @@ import { FormsModule } from '@angular/forms';
 export class AppComponent implements OnInit {
   public title = 'Granja TFM';
   public user: User;
+  public user_register: User;
+
   public identity: any;
   public token: any;
+  public alertRegister: any;
   public errorMessage: any;
 
   constructor(private _userService: UserService) {
-    this.user = new User('', '', '', '', '', 'ROL_user', '');
+    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+    this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+
+    console.log(this.identity);
+    console.log(this.token)
+  }
 
   public onSubmit() {
     console.log(this.user);
@@ -36,26 +46,27 @@ export class AppComponent implements OnInit {
           if (!this.identity._id) {
             alert("El usuario no está correctamente identificado");
           } else {
-            this._userService.signup(this.user, true).subscribe(
-              response => {
-                  let token = response.token;
-                  this.token = token;
-                  if (this.token.length <= 0) {
-                    alert("El token no se ha generado correctamente");
-                  } else {
-                    console.log(token);
-                    console.log(identity);  
+                localStorage.setItem('identity', JSON.stringify(identity));
+                this._userService.signup(this.user, true).subscribe(
+                  response => {
+                      let token = response.token;
+                      this.token = token;
+                      if (this.token.length <= 0) {
+                        alert("El token no se ha generado correctamente");
+                      } else {
+                        localStorage.setItem('token', token);
+                        this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+                      }
+                  },
+                  error => {
+                    if (error.error && error.error.message) {
+                      this.errorMessage = error.error.message;
+                    } else {
+                      this.errorMessage = 'Error desconocido';
+                    }
+                    console.log(error);
                   }
-              },
-              error => {
-                if (error.error && error.error.message) {
-                  this.errorMessage = error.error.message;
-                } else {
-                  this.errorMessage = 'Error desconocido';
-                }
-                console.log(error);
-              }
-            );  
+                );  
           }
       },
       error => {
@@ -67,5 +78,40 @@ export class AppComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  logout(){
+    localStorage.removeItem('identity');
+    localStorage.removeItem('token');
+    localStorage.clear();
+    this.identity = null;
+    this.token = null;
+  }
+
+  
+
+  onSubmitRegister(){
+    console.log(this.user_register);
+    this._userService.register(this.user_register).subscribe(
+      response => {
+          let user = response.user;
+          this.user_register = user;
+          if(!user._id){
+            this.alertRegister = 'Error al registrarse';
+          }else{
+            this.alertRegister = 'El registro se ha realizado correctamente, identificate con '+this.user_register.email;
+            this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
+          }
+      },
+      error => {
+        if (error.error && error.error.message) {
+          this.errorMessage = error.error.message;
+        } else {
+          this.alertRegister = 'Error desconocido';
+        }
+        console.log(error);
+      }
+    );
+
   }
 }
--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,30 +1,36 @@
-
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { map, catchError  } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 import { GLOBAL } from './global';
 import { User } from '../models/user';
-
+import { isPlatformBrowser } from '@angular/common';
+import { Inject, PLATFORM_ID } from '@angular/core';
 
 @Injectable()
 export class UserService {
+  public identity: any;
+  public token: any;
   public url!: string;
-  constructor(private http: HttpClient) {
+
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
     this.url = GLOBAL.url;
   }
 
-  
   signup(user_to_login: User, gethash: boolean | undefined = undefined): Observable<any> {
     user_to_login.gethash = gethash;
     const params = JSON.stringify(user_to_login);
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  
     return this.http.post(this.url + 'login', params, { headers: headers }).pipe(
       map((response: any) => {
         if (response.token) {
           // Guardar el token en algún lugar (por ejemplo, localStorage)
-          localStorage.setItem('token', response.token);
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('token', response.token);
+          }
         }
         return response;
       }),
@@ -32,5 +38,50 @@ export class UserService {
         return throwError(error);
       })
     );
+  }
+
+  register(user_to_register: any){
+    const params = JSON.stringify(user_to_register);
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(this.url + 'register', params, { headers: headers }).pipe(
+      map((response: any) => {
+        if (response.token) {
+          // Guardar el token en algún lugar (por ejemplo, localStorage)
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('token', response.token);
+          }
+        }
+        return response;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        return throwError(error);
+      })
+    );
+  }
+
+  getIdentity() {
+    let identity;
+    if (isPlatformBrowser(this.platformId)) {
+      identity = localStorage.getItem('identity');
+      if (identity) {
+        this.identity = JSON.parse(identity);
+      } else {
+        this.identity = null;
+      }
+    }
+    return this.identity;
+  }
+
+  getToken() {
+    let token;
+    if (isPlatformBrowser(this.platformId)) {
+      token = localStorage.getItem('token');
+      if (token) {
+        this.token = token;
+      } else {
+        this.token = null;
+      }
+    }
+    return this.token;
   }
 }
